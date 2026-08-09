24373c629c98537576122fcf66ebaca0b1439508
Sai-shashank-2005 <saishashankpedapati@gmail.com>
2026-08-09 20:44:03 +0530

    initial event section setup

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,19 +1,22 @@
+// src/data/navigation.ts
+
 export const navigationLinks = [
   {
     label: "Home",
     href: "/",
   },
-
   {
     label: "Services",
     href: "/#services",
   },
-
   {
     label: "About",
     href: "/about",
   },
-
+  {
+    label: "Events",
+    href: "/events",
+  },
   {
     label: "Contact",
     href: "/contact",
